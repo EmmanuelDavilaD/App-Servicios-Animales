49059e7d088b27920e393d905e5352229eecb794
guiaug <guiaug@gmail.com>
2019-04-06 00:43:32 +0200

    servicio de patrocinadores

---
+++ src/app/services/patrocinador.service.ts
@@ -0,0 +1,94 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import {AngularFireAuth} from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  private snapshotChangesSubscription: any;
+
+  constructor(
+    public afs: AngularFirestore
+  ) {
+  }
+
+  unsubscribeOnLogOut() {
+    this.snapshotChangesSubscription.unsubscribe();
+  }
+
+  getPatrocinadores() {
+    return new Promise<any>((resolve, reject) => {
+      this.snapshotChangesSubscription = this.afs.collection('patrocinadores').snapshotChanges();
+      resolve(this.snapshotChangesSubscription);
+    });
+  }
+
+  getPatrocinador(patrocinadorId) {
+    return new Promise<any>((resolve, reject) => {
+      this.snapshotChangesSubscription = this.afs.doc<any>('/patrocinadores/' + patrocinadorId).valueChanges()
+        .subscribe(snapshots => {
+          resolve(snapshots);
+        }, err => {
+          reject(err);
+        });
+    });
+  }
+
+  createPatrocinador(value) {
+    return new Promise<any>((resolve, reject) => {
+      this.afs.collection('patrocinadores').add({
+        image: value.image,
+        url: value.url
+      })
+        .then(
+          res => resolve(res),
+          err => reject(err)
+        );
+    });
+  }
+
+  deletePatrocinador(patrocinadoresKey) {
+    return new Promise<any>((resolve, reject) => {
+      this.afs.collection('patrocinadores').doc(patrocinadoresKey).delete()
+        .then(
+          res => resolve(res),
+          err => reject(err)
+        );
+    });
+  }
+
+  encodeImageUri(imageUri, callback) {
+    var c = document.createElement('canvas');
+    var ctx = c.getContext('2d');
+    var img = new Image();
+    img.onload = function () {
+      var aux: any = this;
+      c.width = aux.width;
+      c.height = aux.height;
+      ctx.drawImage(img, 0, 0);
+      var dataURL = c.toDataURL('image/jpeg');
+      callback(dataURL);
+    };
+    img.src = imageUri;
+  };
+
+  uploadImage(imageURI, randomId) {
+    return new Promise<any>((resolve, reject) => {
+      let storageRef = firebase.storage().ref();
+      let imageRef = storageRef.child('image').child(randomId);
+      this.encodeImageUri(imageURI, function (image64) {
+        imageRef.putString(image64, 'data_url')
+          .then(snapshot => {
+            snapshot.ref.getDownloadURL()
+              .then(res => resolve(res));
+          }, err => {
+            reject(err);
+          });
+      });
+    });
+  }
+}
